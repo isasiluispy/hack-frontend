efdfb95f9830e9e70e90de5442bc020808bfdc66
isasiluispy <isasiluispy@gmail.com>
2024-07-02 22:48:02 -0400

    sidebar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehicleChartComponent } from './vehicle-chart/vehicle-chart.component';
 import { PopulationChartComponent } from './population-chart/population-chart.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, VehicleChartComponent, PopulationChartComponent],
+  imports: [CommonModule, VehicleChartComponent, PopulationChartComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
